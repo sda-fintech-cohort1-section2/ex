4335670d564993c2e02fa2c64bb36246fedae6b6
sda-fintech-cohort1-section2 <91667376+sda-fintech-cohort1-section2@users.noreply.github.com>
2021-12-19 23:04:50 -0500

    Update dapp.js

--- dapp.js
+++ dapp.js
@@ -1,51 +1,51 @@
-// const contractAddress = "0x82BFC222a5A61E2b52146c2393Ce7DA66673A1d8";
+const contractAddress = "0x82BFC222a5A61E2b52146c2393Ce7DA66673A1d8";
 
-// const dApp = {
-// main: async function() {
+const dApp = {
+main: async function() {
     
-//     ethEnabled: function() {
-//     // If the browser has an Ethereum provider (MetaMask) installed
-//     if (window.ethereum) {
-//       window.web3 = new Web3(window.ethereum);
-//       window.ethereum.enable();
-//       return true;
-//     }
-//     return false;
-//   },
+    ethEnabled: function() {
+    // If the browser has an Ethereum provider (MetaMask) installed
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+      window.ethereum.enable();
+      return true;
+    }
+    return false;
+  },
   
-//   setString: async function(newString) {
-//       await this.simpleContract.methods.setString(newString).send({from: this.accounts[0]}).on("receipt", async (receipt) => {
-//       M.toast({ html: "String updated!"});
-//     });
-//     },
-//   getString: async function() {
-//       result = await this.simpleContract.methods.getString().call();
-//       M.toast({ html: "Your String is: ${result}"});
-//   },
-//     if (!this.ethEnabled()) {
-//       alert("Please install MetaMask to use this dApp!");
-//     }
+  setString: async function(newString) {
+      await this.simpleContract.methods.setString(newString).send({from: this.accounts[0]}).on("receipt", async (receipt) => {
+      M.toast({ html: "String updated!"});
+    });
+    },
+  getString: async function() {
+      result = await this.simpleContract.methods.getString().call();
+      M.toast({ html: "Your String is: ${result}"});
+  },
+    if (!this.ethEnabled()) {
+      alert("Please install MetaMask to use this dApp!");
+    }
 
-//     this.accounts = await window.web3.eth.getAccounts();
-//     this.contractAddress = contractAddress;
+    this.accounts = await window.web3.eth.getAccounts();
+    this.contractAddress = contractAddress;
 
-//     this.simpleJson = await (await fetch("./Simple.json")).json();
+    this.simpleJson = await (await fetch("./Simple.json")).json();
     
-//     this.simpleContract = new window.web3.eth.Contract(
-//       this.simpleJson,
-//       this.contractAddress,
-//       { defaultAccount: this.accounts[0] }
-//     );
-//     console.log("Contract object", this.simpleContract);
-// //     await this.updateUI();
-// }
-
-// dApp.main();
-
-
-function buttonClick(){
-    alert("Code entered.");
+    this.simpleContract = new window.web3.eth.Contract(
+      this.simpleJson,
+      this.contractAddress,
+      { defaultAccount: this.accounts[0] }
+    );
+    console.log("Contract object", this.simpleContract);
+//     await this.updateUI();
 }
+
+dApp.main();
+
+
+// function buttonClick(){
+//     alert("Code entered.");
+// }
 
 
 // $(document).ready(function () {
